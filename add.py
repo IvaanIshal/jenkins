a6bdade0d184ba219c5e837613502769f9a0d59f
ivaanishal <nimishanaveen2619@gmail.com>
2025-12-31 11:54:22 +0000

    this is my git commit

--- add.py
+++ add.py
@@ -1,4 +1,16 @@
-num1, num2 = 10, 20
-total = num1 + num2
-content = f"The result of {num1} + {num2} is {total}"
+import sys
+ 
+if len(sys.argv) != 3:
+    print("Usage: python3 test.py <number1> <number2>")
+    sys.exit(1)
+ 
+number1 = float(sys.argv[1])
+number2 = float(sys.argv[2])
+ 
+def add_numbers(num1, num2):
+    return num1 + num2
+ 
+result = add_numbers(number1, number2)
+ 
+print(f"The sum of {number1} and {number2} is: {result}")
 
